7773900921c784b90485b30836917ec1836dd5df
Zack Urben <zackurben@gmail.com>
2020-08-15 16:20:01 -0500

    Re-arranging socket io code for server.

--- public/server.js
+++ public/server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const STATS = true;
 const DEBUG = true;
 const TICK_RATE = 10;
 const TICK_TIME = 1000 / TICK_RATE;
@@ -47,6 +48,21 @@ class Player {
   }
 }
 
+io.on('connection', (socket) => {
+  const player = new Player(socket);
+
+  socket.on('disconnect', () => {
+    state.remPlayer(socket);
+    log('Disconnected', socket.id);
+  });
+
+  socket.on('play', () => {
+    state.addPlayer(socket, player);
+  });
+
+  log('Connected', socket.id);
+});
+
 let delta = 0;
 let elapsed = 0;
 let current = Date.now();
@@ -59,43 +75,28 @@ const tick = () => {
 
   // GAME LOGIC
 
+  // Update the stats and wait for the next tick.
   elapsed = Date.now() - current;
   sleep = Math.max(TICK_TIME - elapsed, 0);
-  log('TICK', Date.now());
+  log('TICK');
   log('delta', delta);
   log('current', current);
   log('last', last);
   log('elapsed', elapsed);
   log('sleep', sleep);
+  STATS && io.emit('stats', { delta, current, last, elapsed, sleep });
   setTimeout(tick, sleep);
 };
 
 // Start the main game loop
 setTimeout(tick, TICK_TIME);
 
+/**
+ * Mount all the API endpoints
+ *
+ * NOTE: DO NOT make an endpoint named 'io', this is non-standard behavior.
+ */
 module.exports = {
-  /**
-   * Socket.io configuration
-   */
-  io: (socket) => {
-    const player = new Player(socket);
-
-    socket.on('disconnect', () => {
-      state.remPlayer(socket);
-      log('Disconnected', socket.id);
-    });
-
-    socket.on('play', () => {
-      state.addPlayer(socket, player);
-    });
-
-    log('Connected', socket.id);
-  },
-
-  /**
-   * API Endpoints.
-   */
-
   // Debug endpoint for server state
   state: (req, res, next) => {
     return res.json({ players, items, colliders });
