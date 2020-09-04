cf0f9e08bea9c0c9e519179658978b3d5a14a04e
Zack Urben <zackurben@gmail.com>
2020-09-03 20:32:47 -0500

    Using paper to figure out weird boxing math.

--- test/MOUSE.js
+++ test/MOUSE.js
@@ -1,6 +1,6 @@
 let x = 0;
 let y = 0;
-let mouseAngleDegrees = 0;
+let mouseAngleDegrees = 225;
 let WEAPON_HEIGHT = 200;
 let WEAPON_WIDTH = 5;
 let WEAPON_RESOLUTION = 5;
@@ -9,6 +9,9 @@ const PRECISION = 10;
 const rad = (d) => (d * Math.PI) / 180;
 const sin = (d) => parseFloat(Math.sin(rad(d)).toFixed(PRECISION));
 const cos = (d) => parseFloat(Math.cos(rad(d)).toFixed(PRECISION));
+const min = (...i) => Math.min(...i);
+const max = (...i) => Math.max(...i);
+const abs = (i) => Math.abs(i);
 
 // get the hit box dimensions
 const len = WEAPON_HEIGHT / WEAPON_RESOLUTION;
@@ -38,27 +41,52 @@ function getWeaponColliders() {
   return out;
 }
 
+// rects are drawn from bottom left to top right
+// when the canvas is centered in the top left as 0,0
 function collidersToRects(colliders) {
-  const wid = WEAPON_WIDTH;
-  let lastX = 0;
-  let lastY = 0;
+  // right
+  if (mouseAngleDegrees === 0) {
+    return [[x, y + WEAPON_WIDTH / 2, WEAPON_HEIGHT, WEAPON_WIDTH]];
+  }
+  // down
+  if (mouseAngleDegrees === 90) {
+    return [[x - WEAPON_WIDTH / 2, y, WEAPON_WIDTH, WEAPON_HEIGHT]];
+  }
+  // left
+  if (mouseAngleDegrees === 180) {
+    return [[x, y - WEAPON_WIDTH / 2, WEAPON_HEIGHT, WEAPON_WIDTH]];
+  }
+  // up
+  if (mouseAngleDegrees === 270) {
+    return [[x + WEAPON_WIDTH / 2, y, WEAPON_WIDTH, WEAPON_HEIGHT]];
+  }
 
   let out = [];
-  colliders.forEach((c) => {
-    let [x, y] = c;
+  let lastX = x;
+  let lastY = y;
+  colliders.forEach((c, i) => {
+    const [cx, cy] = c;
+    const minX = min(abs(cx), abs(lastX));
+    const maxX = max(abs(cx), abs(lastX));
+    const minY = min(abs(cy), abs(lastY));
+    const maxY = max(abs(cy), abs(lastY));
+    const width = maxX - minX;
+    const height = maxY - minY;
 
-    let dx = x - lastX;
-    let dy = y - lastY;
-    // horizontal line
-    if (dy === 0) {
-    }
-    // vertical line
-    if (dx === 0) {
-    }
+    let invertX = cx < 0;
+    let invertY = cy < 0;
 
-    out.push();
+    console.log(i, c);
+    console.log('x', minX, maxX);
+    console.log('width', maxX - minX);
+    console.log('y', minY, maxY);
+    console.log('height', maxY - minY);
+
+    out.push([invertX ? lastX - width : lastX, invertY ? lastY - height : lastY, width, height]);
+    lastX += invertX ? -width : width;
+    lastY += invertY ? -height : height;
   });
   return out;
 }
 
-console.log(getWeaponColliders());
+console.log(collidersToRects(getWeaponColliders()));
