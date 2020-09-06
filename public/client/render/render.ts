b673c6218c70e04445292d2791bcae4452d24108
Zack Urben <zackurben@gmail.com>
2020-09-06 14:15:22 -0500

    Changing the render order to show server on top.

--- public/client/render/render.ts
+++ public/client/render/render.ts
@@ -67,10 +67,6 @@ export function renderGame() {
       const relativeCoordinateX = player.x - screenCoordinate0X;
       const relativeCoordinateY = player.y - screenCoordinate0Y;
 
-      if (SHOW_BOUNDING_BOXES) {
-        renderColliders(ctx, player.colliders, screenCoordinate0X, screenCoordinate0Y);
-      }
-
       renderPlayer(
         ctx,
         relativeCoordinateX,
@@ -83,6 +79,10 @@ export function renderGame() {
         player.id === id ? '#0F9BF2' : '#F25C05',
         'black'
       );
+
+      if (SHOW_BOUNDING_BOXES) {
+        renderColliders(ctx, player.colliders, screenCoordinate0X, screenCoordinate0Y);
+      }
     });
 
   // Render map.
