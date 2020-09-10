b4e54134dea9fc9a7359c985ac34f6339e6dfbe0
Zack Urben <zackurben@gmail.com>
2020-09-10 18:39:44 -0500

    Updating client to use the same diff util and have min frame sleep time.

--- public/client/game-loop.ts
+++ public/client/game-loop.ts
@@ -1,11 +1,10 @@
-import { SHOW_PERFORMANCE_METRICS } from '../shared/variables';
+import { SHOW_PERFORMANCE_METRICS, diff } from '../shared/variables';
 import configuration from './configuration';
 import { getMouseAngle } from './input';
 import { renderGame } from './render/render';
 import { getPlayerState } from './state';
 import { sendDataAction } from './socket/actions';
 import { getSocket } from './socket/socket';
-import { getDiff } from './object-utilities';
 
 // MS per game tick.
 const statsFpsEl = document.getElementById('stats--fps');
@@ -24,16 +23,15 @@ export function tick() {
       mouseAngleDegrees: getMouseAngle(),
     };
 
-    const diff = getDiff(currentPlayerState, updatedPlayerState);
-
-    if (diff != null) {
+    const d = diff(currentPlayerState, updatedPlayerState);
+    if (d != undefined) {
       // Update server with input state.
-      sendDataAction(getSocket(), diff);
+      sendDataAction(getSocket(), d);
     }
   }
 
   // Wait until the next tick.
-  setTimeout(tick, configuration.gameLoopTickMs - (Date.now() - tickStart));
+  setTimeout(tick, Math.max(0, configuration.gameLoopTickMs - (Date.now() - tickStart)));
 }
 
 let lastRenderStartMs = 1;
